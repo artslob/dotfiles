a6b829a4a782e9d5529f0dbe50b9e79c6a6b34d6
artslob <artslob@yandex.ru>
2019-06-11 23:05:54 +0300

    platform can include more that 1 directory

--- bash/.bash-config/bashrc.sh
+++ bash/.bash-config/bashrc.sh
@@ -4,26 +4,27 @@ CURRENT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 get_platform() {
     local PLATFORM_FILE="${CURRENT_DIR}/platform"
-    local DEFAULT_PLATFORM='desktop'
+    local DEFAULT_PLATFORM='default'
+
+    echo "$DEFAULT_PLATFORM"
 
     if [[ ! -r "$PLATFORM_FILE" ]]; then
-        echo -n "$DEFAULT_PLATFORM"
         return
     fi
-    PLATFORM="$(cat "$PLATFORM_FILE")"
-    if [[ ! -d "${CURRENT_DIR}/${PLATFORM}" ]]; then
-        echo -n "$DEFAULT_PLATFORM"
-        return
-    fi
-    echo -n "$PLATFORM"
+    cat "$PLATFORM_FILE" | while read -r dir; do
+        if [[ "$dir" != "$DEFAULT_PLATFORM" && -d "${CURRENT_DIR}/${dir}" ]]; then
+            echo "$dir"
+        fi
+    done
 }
 
 PLATFORM=`get_platform`
 
 get_config_value() {
     (
-        source "${CURRENT_DIR}/default/config.sh"
-        source "${CURRENT_DIR}/${PLATFORM}/config.sh"
+        for dir in $(echo $PLATFORM); do
+            source "${CURRENT_DIR}/${dir}/config.sh"
+        done
         if [[ -e "${CURRENT_DIR}/local/config.sh" ]]; then
             source "${CURRENT_DIR}/local/config.sh"
         fi
@@ -36,12 +37,13 @@ PS1=$("${CURRENT_DIR}/set-ps-1.sh" \
         "$(get_config_value 'PS1_INCLUDE_HOST')" \
         )
 
-for file in namespace.sh aliases.sh; do
-    source "${CURRENT_DIR}/default/${file}"
-    source "${CURRENT_DIR}/${PLATFORM}/${file}"
-    if [[ -r "${CURRENT_DIR}/local/${file}" ]]; then
-        source "${CURRENT_DIR}/local/${file}"
-    fi
+for dir in $(echo $PLATFORM); do
+    for file in namespace.sh aliases.sh; do
+        source "${CURRENT_DIR}/${dir}/${file}"
+        if [[ -r "${CURRENT_DIR}/local/${file}" ]]; then
+            source "${CURRENT_DIR}/local/${file}"
+        fi
+    done
 done
 
 if [ "$(get_config_value 'SSH_AGENT_SETUP')" == "yes" ]; then
