a802d7cf68156c00c6b14f131d99a9c9ee7e3424
artslob <artslob@yandex.ru>
2019-09-08 13:23:20 +0300

    use bg-max flag instead of bg-scale for feh

--- i3/.config/i3/wallpaper.sh
+++ i3/.config/i3/wallpaper.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# make it a symbolic link to page
+# e.g. ln -fs /path/to/image.png ~/.wallpaper
 wp="${HOME}/.wallpaper"
 
 if [[ -r "$wp" ]]; then
-    feh --bg-scale "$wp"
+    feh --bg-max "$wp"
 fi
