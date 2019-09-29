ba95cf7984f3995c2473b443fa1d3deb3dd07832
artslob <artslob@yandex.ru>
2019-09-29 10:51:53 +0300

    if script .fehbg exists, run it and exit

--- i3/.config/i3/wallpaper.sh
+++ i3/.config/i3/wallpaper.sh
@@ -4,6 +4,14 @@
 # e.g. ln -fs /path/to/image.png ~/.wallpaper
 wp="${HOME}/.wallpaper"
 
+# unless --no-fehbg option is passed, feh will create script to set the current background to ~/.fehbg
+feh_script="${HOME}/.fehbg"
+
+if [[ -x "$feh_script" ]]; then
+    "$feh_script"
+    exit 0
+fi
+
 if [[ -r "$wp" ]]; then
     feh --bg-max "$wp"
 fi
