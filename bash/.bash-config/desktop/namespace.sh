28873e2e34d358645df816538add35c58ac402aa
artslob <artslob@yandex.ru>
2019-12-29 19:05:09 +0300

    add funcs to enable/disable touchpad

--- bash/.bash-config/desktop/namespace.sh
+++ bash/.bash-config/desktop/namespace.sh
@@ -8,3 +8,7 @@ restart_kde() { killall plasmashell && kstart plasmashell; }
 browser-with-proxy() {
     ssh -f -N -D 7777 vps 2>&1 /dev/null && vivaldi --proxy-server="socks5://127.0.0.1:7777" &
 }
+
+touchpad_disable() { xinput disable "ETPS/2 Elantech Touchpad"; }
+
+touchpad_enable() { xinput enable "ETPS/2 Elantech Touchpad"; }
