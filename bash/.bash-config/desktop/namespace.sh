f43688868ffafafc12ad48d794329933ba846037
artslob <artslob@yandex.ru>
2022-08-29 23:18:44 +0300

    fix func for vivaldi with proxy

--- bash/.bash-config/desktop/namespace.sh
+++ bash/.bash-config/desktop/namespace.sh
@@ -5,8 +5,8 @@ export GOPATH="${HOME}/go"
 
 restart_kde() { killall plasmashell && kstart plasmashell; }
 
-browser-with-proxy() {
-    ssh -f -N -D 7777 vps 2>&1 /dev/null && vivaldi --proxy-server="socks5://127.0.0.1:7777" &
+vivaldi-with-proxy() {
+    ssh -f -N -D 7777 vps > /dev/null 2>&1 && vivaldi --proxy-server="socks5://127.0.0.1:7777" --password-store=kwallet &
 }
 
 touchpad_disable() { xinput disable "ETPS/2 Elantech Touchpad"; }
